2d6f75359795ebcd020ee7558fd15378232e6c45
almunirah <almunirah.alam@gmail.com>
2026-08-10 12:27:14 +0800

    Add acquisition cataloguing dashboard route

---
+++ app/acquisition-cataloguing/page.tsx
@@ -0,0 +1,2 @@
+import {LibraryDashboard} from '@/components/library-dashboard';
+export default function Page(){return <div data-module-shell="acquisition-cataloguing"><LibraryDashboard initialModule="acquisition-cataloguing"/></div>}

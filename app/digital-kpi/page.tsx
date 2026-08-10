70ae6f0ff77630a2be1e6db2d2b764af073dff72
almunirah <almunirah.alam@gmail.com>
2026-08-10 12:27:19 +0800

    Add digital KPI dashboard route

---
+++ app/digital-kpi/page.tsx
@@ -0,0 +1,2 @@
+import {LibraryDashboard} from '@/components/library-dashboard';
+export default function Page(){return <div data-module-shell="digital-kpi"><LibraryDashboard initialModule="digital-kpi"/></div>}
